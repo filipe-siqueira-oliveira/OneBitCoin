263341a1c3e5c10a4f50ba13b7b8df51a94bc622
Filipe Siqueira <fcsiqueira1409@gmail.com>
2023-06-26 17:03:51 -0300

    implemented APIs

--- App.js
+++ App.js
@@ -5,6 +5,47 @@ import HistoryGraphic from './src/components/HistoryGraphic/'
 import QuotationsList from './src/components/QuotationsList';
 import QuotationsItens from './src/components/QuotationsList/QuotationsItens';
 
+function addZero(number) {
+  if(number <= 9) {
+    return "0" + number
+  }
+  return number
+}
+
+function url(qtdDays) {
+  const date = new Date();
+  const listLastDays = qtdDays;
+  const end_date = `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`;
+  date.setDate(date.getDate() - listLastDays);
+  const start_date = `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`;
+  return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start_date}&end=${end_date}`
+}
+
+async function getListCoins(url) {
+  let response = await fetch(url);
+  let returnApi = await response.json();
+  let selectListQuotations = returnApi.bpi
+  const queryCoinsList = Object.keys(selectListQuotations).map((key) => {
+    return {
+      data: key.split("-").reverse().join("/"),
+      valor: selectListQuotations[key],
+    }
+  })
+  let data = queryCoinsList.reverse();
+  return data
+}
+
+async function getPriceCoinsGraphic(url) {
+  let responseG = await fetch(url);
+  let returnApiG = await responseG.json();
+  let selectListQuotationsG = returnApiG.bpi
+  const queryCoinsList = Object.keys(selectListQuotationsG).map((key) => {
+    selectListQuotationsG[key]
+  });
+  let dataG = queryCoinsList.reverse();
+  return dataG
+}
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
